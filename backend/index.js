1525e2aa962595370bb1c093a087a43835777022
Santonu-Naskar <95866317+Santonu-Naskar@users.noreply.github.com>
2023-03-23 19:45:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,11 @@ const port = process.env.PORT|| 5000;
 
 app.use(cors())
 app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials","true");
+  res.send("API is running..");
+});
 // available routes
 app.use('/auth',require('./routes/auth'))
 app.use('/notes',require('./routes/notes'))
